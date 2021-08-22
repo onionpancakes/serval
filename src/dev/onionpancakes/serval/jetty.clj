28333da685b725b5f87e57c6d7d9cda5fc775d5f
onionpancakes <639985+onionpancakes@users.noreply.github.com>
2021-08-21 22:58:17 -0700

    exposed more connector options

--- src/dev/onionpancakes/serval/jetty.clj
+++ src/dev/onionpancakes/serval/jetty.clj
@@ -12,32 +12,43 @@
 
 (defn http-configuration
   [config]
-  (let [http-config (HttpConfiguration.)]
-    (if-let [ssv? (:send-server-version? config)]
-      (.setSendServerVersion http-config ssv?))
-    http-config))
+  (let [hconf (HttpConfiguration.)]
+    (if (contains? config :send-date-header?)
+      (.setSendDateHeader hconf (:send-date-header? config)))
+    (if (contains? config :send-server-version?)
+      (.setSendServerVersion hconf (:send-server-version? config)))
+    (if (contains? config :request-header-size)
+      (.setRequestHeaderSize hconf (:request-header-size config)))
+    (if (contains? config :response-header-size)
+      (.setResponseHeaderSize hconf (:response-header-size config)))
+    (if (contains? config :output-buffer-size)
+      (.setOutputBufferSize hconf (:output-buffer-size config)))
+    hconf))
 
 (defmulti connection-factories :protocol)
 
 (defmethod connection-factories :http
   [config]
-  (let [http-config (http-configuration config)
-        http1       (HttpConnectionFactory. http-config)]
+  (let [hconf (http-configuration config)
+        http1 (HttpConnectionFactory. hconf)]
     [http1]))
 
 (defmethod connection-factories :http2c
   [config]
-  (let [http-config (http-configuration config)
-        http1       (HttpConnectionFactory. http-config)
-        http2       (HTTP2CServerConnectionFactory. http-config)]
+  (let [hconf (http-configuration config)
+        http1 (HttpConnectionFactory. hconf)
+        http2 (HTTP2CServerConnectionFactory. hconf)]
     [http1 http2]))
 
 (defn configure-connector!
   [conn config]
-  (doto conn
-    (.setConnectionFactories (connection-factories config))
-    (cond-> (:port config) (.setPort (:port config))
-            (:host config) (.setHost (:host config)))))
+  (.setConnectionFactories conn (connection-factories config))
+  (if (contains? config :port)
+    (.setPort conn (:port config)))
+  (if (contains? config :host)
+    (.setHost conn (:host config)))
+  (if (contains? config :idle-timeout)
+    (.setIdleTimeout conn (:idle-timeout config))))
 
 ;; Servlet
 
