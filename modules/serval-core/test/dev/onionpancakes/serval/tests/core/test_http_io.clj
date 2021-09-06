e12bba58463a5c9a1e94e060e79c9d99a771efb8
onionpancakes <639985+onionpancakes@users.noreply.github.com>
2021-09-05 19:37:14 -0700

    more request lookup tests

--- modules/serval-core/test/dev/onionpancakes/serval/tests/core/test_http_io.clj
+++ modules/serval-core/test/dev/onionpancakes/serval/tests/core/test_http_io.clj
@@ -4,22 +4,31 @@
             [clojure.test :refer [deftest is are]]))
 
 (def servlet-request-data
-  {:attributes    {"attr1" "foobar-attr"}
-   :remote-addr   "some remote addr"
-   :remote-host   "some remote host"
-   :remote-port   3000
-   :local-addr    "some local addr"
-   :local-name    "some local name"
-   :local-port    3001
-   :scheme        "http"
-   :server-name   "some server name"
-   :server-port   3002
-   :path          "/somepath"
-   :context-path  "/somecontextpath"
-   :servlet-path  "/someservletpath"
-   :path-info     "/somepathinfo"
-   :query-string  "?somequerystring=foo"
-   :parameter-map {"somequerystring" ["foo"]}})
+  {:attributes         {"attr1" "foobar-attr"}
+   :remote-addr        "some remote addr"
+   :remote-host        "some remote host"
+   :remote-port        3000
+   :local-addr         "some local addr"
+   :local-name         "some local name"
+   :local-port         3001
+   :scheme             "http"
+   :server-name        "some server name"
+   :server-port        3002
+   :path               "/somepath"
+   :context-path       "/somecontextpath"
+   :servlet-path       "/someservletpath"
+   :path-info          "/somepathinfo"
+   :query-string       "?somequerystring=foo"
+   :parameter-map      {"somequerystring" ["foo"]}
+   :protocol           "HTTP/1.1"
+   :method             "GET"
+   :headers            {"Content-Type" ["text/plain"]}
+   :content-length     0
+   :content-type       "application/javascript"
+   :character-encoding "UTF-8"
+   ;; Test locales?
+
+   })
 
 (def request
   (-> servlet-request-data
@@ -28,23 +37,29 @@
 
 (deftest test-servlet-request-proxy-lookup
   (are [path expected] (= (get-in request path) expected)
-    [:attributes "attr1"] "foobar-attr"
-    [:attribute-names]    ["attr1"]
-    [:remote-addr]        "some remote addr"
-    [:remote-host]        "some remote host"
-    [:remote-port]        3000
-    [:local-addr]         "some local addr"
-    [:local-name]         "some local name"
-    [:local-port]         3001
-    [:scheme]             "http"
-    [:server-name]        "some server name"
-    [:server-port]        3002
-    [:path]               "/somepath"
-    [:context-path]       "/somecontextpath"
-    [:servlet-path]       "/someservletpath"
-    [:path-info]          "/somepathinfo"
-    [:query-string]       "?somequerystring=foo"
-    [:parameter-map]      {"somequerystring" ["foo"]}))
+    [:attributes "attr1"]       "foobar-attr"
+    [:attribute-names]          ["attr1"]
+    [:remote-addr]              "some remote addr"
+    [:remote-host]              "some remote host"
+    [:remote-port]              3000
+    [:local-addr]               "some local addr"
+    [:local-name]               "some local name"
+    [:local-port]               3001
+    [:scheme]                   "http"
+    [:server-name]              "some server name"
+    [:server-port]              3002
+    [:path]                     "/somepath"
+    [:context-path]             "/somecontextpath"
+    [:servlet-path]             "/someservletpath"
+    [:path-info]                "/somepathinfo"
+    [:query-string]             "?somequerystring=foo"
+    [:parameter-map]            {"somequerystring" ["foo"]}
+    [:protocol]                 "HTTP/1.1"
+    [:method]                   :GET
+    [:headers "Content-Type" 0] "text/plain"
+    [:content-length]           0
+    [:content-type]             "application/javascript"
+    [:character-encoding]       "UTF-8"))
 
 (deftest test-write-response
   (is false))
