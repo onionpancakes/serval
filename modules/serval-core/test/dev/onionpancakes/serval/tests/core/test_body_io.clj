6de72b1322989492dc9f5e4dd109f36cddbb8be8
onionpancakes <639985+onionpancakes@users.noreply.github.com>
2021-09-10 19:20:53 -0700

    migrate body tests to use new mock classes

--- modules/serval-core/test/dev/onionpancakes/serval/tests/core/test_body_io.clj
+++ modules/serval-core/test/dev/onionpancakes/serval/tests/core/test_body_io.clj
@@ -1,6 +1,6 @@
 (ns dev.onionpancakes.serval.tests.core.test-body-io
   (:require [dev.onionpancakes.serval.io.body :as b]
-            [dev.onionpancakes.serval.mock :as mock]
+            [dev.onionpancakes.serval.mock.http :as mock]
             [clojure.test :refer [deftest is]])
   (:import [java.io ByteArrayInputStream]
            [java.util.concurrent CompletionStage]))
@@ -14,20 +14,25 @@
 (deftest test-sync-writes
 
   ;; Bytes
-  (let [resp    (mock/mock-http-servlet-response)
+  (let [req     (mock/http-servlet-request {} "")
+        resp    (mock/http-servlet-response {} req)
         ctx     {:serval.service/response resp}
         _       (b/write-body (.getBytes "Foobar" "utf-8") ctx)
         written (.toByteArray (:output-stream resp))]
     (is (= "Foobar" (String. written "utf-8"))))
 
   ;; String
-  (let [resp (mock/mock-http-servlet-response)
+  (let [req  (mock/http-servlet-request {} "")
+        resp (mock/http-servlet-response {} req)
         ctx  {:serval.service/response resp}
-        _    (b/write-body "Foobar" ctx)]
-    (is (= "Foobar" (str (:writer resp)))))
+        _    (b/write-body "Foobar" ctx)
+        ;; Flush
+        _    (.flush (.getWriter resp))]
+    (is (= "Foobar" (slurp (.toByteArray (:output-stream resp))))))
 
   ;; InputStream
-  (let [resp    (mock/mock-http-servlet-response)
+  (let [req     (mock/http-servlet-request {} "")
+        resp    (mock/http-servlet-response {} req)
         ctx     {:serval.service/response resp}
         in      (ByteArrayInputStream. (.getBytes "Foobar" "utf-8"))
         _       (b/write-body in ctx)
@@ -35,7 +40,8 @@
     (is (= "Foobar" (String. written "utf-8"))))
 
   ;; nil
-  (let [resp (mock/mock-http-servlet-response)
+  (let [req  (mock/http-servlet-request {} "")
+        resp (mock/http-servlet-response {} req)
         ctx  {:serval.service/response resp}
         _    (b/write-body nil ctx)]
     (is (== 0 (alength (.toByteArray (:output-stream resp)))))
@@ -43,37 +49,37 @@
 
 (deftest test-async-read
   ;; Read bytes
-  (let [req (mock/mock-http-servlet-request-string {} "Foobar" "utf-8")
+  (let [req (mock/http-servlet-request {} "Foobar" {:encoding "utf-8"})
         res (.get (b/read-body-as-bytes-async! req))]
     (is (= "Foobar" (String. res "utf-8"))))
 
   ;; Read string
-  (let [req (mock/mock-http-servlet-request-string {} "Foobar" "utf-8")
+  (let [req (mock/http-servlet-request {} "Foobar" {:encoding "utf-8"})
         res (.get (b/read-body-as-string-async! req))]
     (is (= "Foobar" res)))
   
-  ;; Should fail without encoding set on request.
-  (let [req (mock/mock-http-servlet-request-string {} "やばい" "utf-16")
+  ;; Encoding not set
+  (let [req (mock/http-servlet-request {} "Foobar")
         res (.get (b/read-body-as-string-async! req))]
-    (is (not= "やばい" res)))
+    (is (= "Foobar" res)))
   
   ;; Works with encoding set.
-  (let [req (mock/mock-http-servlet-request-string {:character-encoding "utf-16"}
-                                                   "やばい" "utf-16")
+  (let [req (-> {:character-encoding "utf-16"}
+                (mock/http-servlet-request "やばい" {:encoding "utf-16"}))
         res (.get (b/read-body-as-string-async! req))]
     (is (= "やばい" res)))
 
   ;; Should fail when async not supported.
-  (let [req (mock/mock-http-servlet-request-string {:async-supported? false}
-                                                   "Foobar" "utf-8")]
+  (let [req (mock/http-servlet-request {:async-supported? false} "Foobar")]
     (is (thrown? IllegalStateException (b/read-body-as-string-async! req)))))
 
 (deftest test-async-write
   ;; Bytes
-  (let [req  (mock/mock-http-servlet-request-string {} "" "utf-8")
-        resp (mock/mock-http-servlet-response {} req)
+  (let [req  (mock/http-servlet-request {} "")
+        resp (mock/http-servlet-response {} req)
         ctx  {:serval.service/request  req
               :serval.service/response resp}
+        _    (.startAsync req)
         body (b/async-body (.getBytes "Foobar" "utf-8"))
         ret  (b/write-body body ctx)
         res  (.toByteArray (:output-stream resp))]
@@ -82,37 +88,41 @@
     (is (= "Foobar" (String. res "utf-8"))))
 
   ;; String with default encoding.
-  (let [req  (mock/mock-http-servlet-request-string {} "" "utf-8")
-        resp (mock/mock-http-servlet-response {} req)
+  (let [req  (mock/http-servlet-request {} "")
+        resp (mock/http-servlet-response {} req)
         ctx  {:serval.service/request  req
               :serval.service/response resp}
+        _    (.startAsync req)
         body (b/async-body "Foobar")
         ret  (b/write-body body ctx)
-        res  (.toByteArray (:output-stream resp))]
+        res  (slurp (.toByteArray (:output-stream resp)) :encoding "ISO-8859-1")]
     (is (b/async-body? body ctx))
     (is (instance? CompletionStage ret))
-    (is (= "Foobar" (String. res "ISO-8859-1"))))
+    (is (= "Foobar" res)))
 
   ;; String with encoding.
-  (let [req  (mock/mock-http-servlet-request-string {} "" "utf-8")
-        resp (mock/mock-http-servlet-response {:character-encoding "utf-16"} req)
+  (let [req  (mock/http-servlet-request {} "")
+        resp (mock/http-servlet-response {:character-encoding "utf-16"} req)
         ctx  {:serval.service/request  req
               :serval.service/response resp}
+        _    (.startAsync req)
         body (b/async-body "Foobar")
         ret  (b/write-body body ctx)
-        res  (.toByteArray (:output-stream resp))]
+        res  (slurp (.toByteArray (:output-stream resp)) :encoding "utf-16")]
     (is (b/async-body? body ctx))
     (is (instance? CompletionStage ret))
-    (is (= "Foobar" (String. res "utf-16"))))
+    (is (= "Foobar" res)))
 
   ;; String with async disabled
-  (let [req  (mock/mock-http-servlet-request-string {:async-supported? false} "" "utf-8")
-        resp (mock/mock-http-servlet-response {} req)
+  (let [req  (mock/http-servlet-request {:async-supported? false} "")
+        resp (mock/http-servlet-response {} req)
         ctx  {:serval.service/request  req
               :serval.service/response resp}
         body (b/async-body "Foobar")
         ret  (b/write-body body ctx)
-        res  (str (:writer resp))]
+        ;; Flush the writer.
+        _    (.flush (.getWriter resp))
+        res  (slurp (.toByteArray (:output-stream resp)))]
     (is (not (b/async-body? body ctx)))
     (is (nil? ret))
     (is (= "Foobar" res))))
