67eaea0c976fd55d8cd4c7f4eaa14a5d1dba8571
onionpancakes <639985+onionpancakes@users.noreply.github.com>
2021-09-10 18:57:38 -0700

    tests for middleware

---
+++ modules/serval-core/test/dev/onionpancakes/serval/tests/core/test_middleware.clj
@@ -0,0 +1,47 @@
+(ns dev.onionpancakes.serval.tests.core.test-middleware
+  (:require [dev.onionpancakes.serval.core :as c]
+            [clojure.test :refer [deftest is are]]))
+
+(def echo
+  (c/handler (c/map identity)))
+
+(deftest test-echo
+  (are [input expected] (= (echo input) expected)
+    nil         nil
+    :foo        :foo
+    0           0
+    {:foo :bar} {:foo :bar}))
+
+(def times2-plus1
+  (c/handler (comp (c/map * 2)
+                   (c/map + 1))))
+
+(deftest test-times2-plus1
+  (are [input expected] (= (times2-plus1 input) expected)
+    0  1
+    1  3
+    2  5
+    -1 -1))
+
+(def terminate-on-key
+  (c/handler (comp (c/terminate :stop assoc :aborted? true)
+                   (c/map assoc :foo :bar))))
+
+(deftest test-terminate-with-key
+  (are [x y] (= (terminate-on-key x) y)
+    {}            {:foo :bar}
+    {:stop true}  {:stop true :aborted? true}
+    {:stop false} {:stop false :foo :bar}
+    nil           {:foo :bar}))
+
+(def terminate-and-continue
+  (c/handler (comp (c/terminate :stop identity)
+                   (c/map assoc :foo :bar))
+             (comp (c/terminate :stop2 identity)
+                   (c/map assoc :foo2 :bar2))))
+
+(deftest test-terminate-and-continue
+  (are [x y] (= (terminate-and-continue x) y)
+    {}                       {:foo  :bar :foo2 :bar2}
+    {:stop true}             {:stop true :foo2 :bar2}
+    {:stop true :stop2 true} {:stop true :stop2 true}))
