8fb2b697e2eaa362869d9ddebc1405a48de8f73f
onionpancakes <639985+onionpancakes@users.noreply.github.com>
2022-03-28 17:11:50 -0700

    Refactor jetty2 to use with-server macro

--- modules/serval-jetty/test/dev/onionpancakes/serval/tests/jetty/test_jetty2.clj
+++ modules/serval-jetty/test/dev/onionpancakes/serval/tests/jetty/test_jetty2.clj
@@ -2,46 +2,78 @@
   (:refer-clojure :exclude [send])
   (:require [dev.onionpancakes.serval.jetty2 :as j]
             [clojure.test :refer [is deftest use-fixtures]])
-  (:import [java.net.http HttpClient HttpRequest
+  (:import [java.net.http HttpClient HttpRequest HttpResponse
             HttpRequest$BodyPublishers HttpResponse$BodyHandlers]
            [java.net URI]
            [org.eclipse.jetty.server Server]))
 
-(defn handler
-  [ctx]
-  {:serval.response/body "foo"})
-
-(def ^Server server
-  (j/server {:connectors [{:port 42000}]
-             :handler    handler}))
-
-(defn with-server-started-fixture
-  [f]
-  (.start server)
-  (f)
-  (.stop server))
-
-(use-fixtures :once with-server-started-fixture)
-
 ;; Http client
 
-(def client
+(def ^HttpClient client
   (.build (HttpClient/newBuilder)))
 
-(def request
+(defn to-request
+  [uri]
   (-> (HttpRequest/newBuilder)
-      (.uri (URI. "http://localhost:42000"))
+      (.uri (URI. uri))
       (.method "GET" (HttpRequest$BodyPublishers/noBody))
       (.build)))
 
-(defn send
-  [^HttpClient client req]
-  (let [resp (.send client req (HttpResponse$BodyHandlers/ofString))]
-    {:status (.statusCode resp)
-     :body   (.body resp)}))
+(defn ^HttpResponse send
+  [req]
+  (.send client (to-request req) (HttpResponse$BodyHandlers/ofString)))
+
+;; Server
+
+(defmacro with-server
+  "Creates a server with config running in scope of expression."
+  [config & body]
+  `(let [server# (j/server ~config)]
+     (.start server#)
+     ~@body
+     (.stop server#)))
 
 ;; Tests
 
-(deftest test-server
-  (let [resp (send client request)]
-    (is (= {:status 200 :body "foo"} resp))))
+(deftest test-minimal
+  (with-server {:connectors [{:port 42000}]
+                :handler    (constantly {:serval.response/body "foo"})}
+    (let [resp (send "http://localhost:42000")]
+      (is (= (str (.version resp)) "HTTP_1_1"))
+      (is (= (.statusCode resp) 200))
+      (is (= (.body resp) "foo")))))
+
+(deftest test-http1
+  (with-server {:connectors [{:protocol :http
+                              :port     42000}]
+                :handler    (constantly {:serval.response/body "foo"})}
+    (let [resp (send "http://localhost:42000")]
+      (is (= (str (.version resp)) "HTTP_1_1"))
+      (is (= (.statusCode resp) 200))
+      (is (= (.body resp) "foo")))))
+
+(deftest test-http2c
+  (with-server {:connectors [{:protocol :http2c
+                              :port     42000}]
+                :handler    (constantly {:serval.response/body "foo"})}
+    (let [resp (send "http://localhost:42000")]
+      (is (= (str (.version resp)) "HTTP_2"))
+      (is (= (.statusCode resp) 200))
+      (is (= (.body resp) "foo")))))
+
+(deftest test-multiple-connectors
+  (with-server {:connectors [{:protocol :http
+                              :port     42000}
+                             {:protocol :http2c
+                              :port     42001}]
+                :handler    #(case (int (:server-port (:serval.service/request %)))
+                               42000 {:serval.response/body "foo"}
+                               42001 {:serval.response/body "bar"})}
+    (let [resp (send "http://localhost:42000")]
+      (is (= (str (.version resp)) "HTTP_1_1"))
+      (is (= (.statusCode resp) 200))
+      (is (= (.body resp) "foo")))
+    (let [resp (send "http://localhost:42001")]
+      (is (= (str (.version resp)) "HTTP_2"))
+      (is (= (.statusCode resp) 200))
+      (is (= (.body resp) "bar")))))
