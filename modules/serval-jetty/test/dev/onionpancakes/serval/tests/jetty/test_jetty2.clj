f1a32089713377ed5a70eb2b09b071e31364df10
onionpancakes <639985+onionpancakes@users.noreply.github.com>
2022-03-28 17:35:16 -0700

    Added gzip-handler test; refactor send to use a map instead to support headers

--- modules/serval-jetty/test/dev/onionpancakes/serval/tests/jetty/test_jetty2.clj
+++ modules/serval-jetty/test/dev/onionpancakes/serval/tests/jetty/test_jetty2.clj
@@ -3,25 +3,40 @@
   (:require [dev.onionpancakes.serval.jetty2 :as j]
             [clojure.test :refer [is deftest use-fixtures]])
   (:import [java.net.http HttpClient HttpRequest HttpResponse
+            HttpRequest$Builder
             HttpRequest$BodyPublishers HttpResponse$BodyHandlers]
            [java.net URI]
-           [org.eclipse.jetty.server Server]))
+           [java.io ByteArrayInputStream]
+           [java.util.zip GZIPInputStream]
+           [org.eclipse.jetty.server Server]
+           [org.eclipse.jetty.server.handler.gzip GzipHandler]))
 
 ;; Http client
 
 (def ^HttpClient client
   (.build (HttpClient/newBuilder)))
 
+(defn ^HttpRequest$Builder set-headers
+  [^HttpRequest$Builder builder headers]
+  (doseq [[header-key values] headers
+          :let                [header-name (name header-key)]
+          value               values]
+    (.setHeader builder header-name value))
+  builder)
+
 (defn to-request
-  [uri]
+  [req]
   (-> (HttpRequest/newBuilder)
-      (.uri (URI. uri))
+      (.uri (URI. (:uri req)))
       (.method "GET" (HttpRequest$BodyPublishers/noBody))
+      (set-headers (:headers req))
       (.build)))
 
 (defn ^HttpResponse send
-  [req]
-  (.send client (to-request req) (HttpResponse$BodyHandlers/ofString)))
+  ([req]
+   (.send client (to-request req) (HttpResponse$BodyHandlers/ofString)))
+  ([req bh]
+   (.send client (to-request req) bh)))
 
 ;; Server
 
@@ -38,7 +53,7 @@
 (deftest test-minimal
   (with-server {:connectors [{:port 42000}]
                 :handler    (constantly {:serval.response/body "foo"})}
-    (let [resp (send "http://localhost:42000")]
+    (let [resp (send {:uri "http://localhost:42000"})]
       (is (= (str (.version resp)) "HTTP_1_1"))
       (is (= (.statusCode resp) 200))
       (is (= (.body resp) "foo")))))
@@ -47,7 +62,7 @@
   (with-server {:connectors [{:protocol :http
                               :port     42000}]
                 :handler    (constantly {:serval.response/body "foo"})}
-    (let [resp (send "http://localhost:42000")]
+    (let [resp (send {:uri "http://localhost:42000"})]
       (is (= (str (.version resp)) "HTTP_1_1"))
       (is (= (.statusCode resp) 200))
       (is (= (.body resp) "foo")))))
@@ -56,7 +71,7 @@
   (with-server {:connectors [{:protocol :http2c
                               :port     42000}]
                 :handler    (constantly {:serval.response/body "foo"})}
-    (let [resp (send "http://localhost:42000")]
+    (let [resp (send {:uri "http://localhost:42000"})]
       (is (= (str (.version resp)) "HTTP_2"))
       (is (= (.statusCode resp) 200))
       (is (= (.body resp) "foo")))))
@@ -69,11 +84,27 @@
                 :handler    #(case (int (:server-port (:serval.service/request %)))
                                42000 {:serval.response/body "foo"}
                                42001 {:serval.response/body "bar"})}
-    (let [resp (send "http://localhost:42000")]
+    (let [resp (send {:uri "http://localhost:42000"})]
       (is (= (str (.version resp)) "HTTP_1_1"))
       (is (= (.statusCode resp) 200))
       (is (= (.body resp) "foo")))
-    (let [resp (send "http://localhost:42001")]
+    (let [resp (send {:uri "http://localhost:42001"})]
       (is (= (str (.version resp)) "HTTP_2"))
       (is (= (.statusCode resp) 200))
       (is (= (.body resp) "bar")))))
+
+(defn decompress
+  "Decompress bytes into a String."
+  [^bytes b]
+  (slurp (GZIPInputStream. (ByteArrayInputStream. b))))
+
+(deftest test-gzip-handler
+  (with-server {:connectors [{:protocol :http
+                              :port     42000}]
+                :handler    (-> (constantly {:serval.response/body "foo"})
+                                (j/gzip-handler {:min-gzip-size 0}))}
+    (let [resp (send {:uri     "http://localhost:42000"
+                      :headers {:accept-encoding ["gzip"]}}
+                     (HttpResponse$BodyHandlers/ofByteArray))]
+      (is (= (.statusCode resp) 200))
+      (is (= (decompress (.body resp)) "foo")))))
