ad5f7933014cd62984df43d3411e9a1114b3fa56
onionpancakes <639985+onionpancakes@users.noreply.github.com>
2022-03-31 21:32:01 -0700

    Initial http2; request impl

---
+++ modules/serval-core/src/dev/onionpancakes/serval/io/http2.clj
@@ -0,0 +1,104 @@
+(ns dev.onionpancakes.serval.io.http2
+  (:require [dev.onionpancakes.serval.io.body2 :as body])
+  (:import [java.util.concurrent CompletionStage CompletableFuture]
+           [java.util.function Function]
+           [jakarta.servlet.http
+            HttpServletRequest
+            HttpServletResponse
+            HttpServletRequestWrapper]))
+
+;; Request
+
+(deftype Attributes [^HttpServletRequest request]
+  clojure.lang.ILookup
+  (valAt [this k]
+    (.valAt this key nil))
+  (valAt [this k not-found]
+    ;; Can't use 'or' function because ret might be false.
+    (if-some [val (.getAttribute request k)]
+      val
+      not-found)))
+
+(deftype Headers [^HttpServletRequest request]
+  clojure.lang.ILookup
+  (valAt [this key]
+    (.valAt this key nil))
+  (valAt [this key not-found]
+    (if-some [val (some->> (.getHeaders request key)
+                           (enumeration-seq)
+                           (vec))]
+      val
+      not-found)))
+
+(defn servlet-request-lookup
+  [^HttpServletRequest request key not-found]
+  (case key
+    ;; Attributes
+    :attributes      (Attributes. request)
+    :attribute-names (some->> (.getAttributeNames request)
+                              (enumeration-seq)
+                              (vec))
+
+    :remote-addr (.getRemoteAddr request)
+    :remote-host (.getRemoteHost request)
+    :remote-port (.getRemotePort request)
+
+    :local-addr (.getLocalAddr request)
+    :local-name (.getLocalName request)
+    :local-port (.getLocalPort request)
+
+    :dispatcher-type (.getDispatcherType request)
+
+    ;; URL
+    :scheme        (.getScheme request)
+    :server-name   (.getServerName request)
+    :server-port   (.getServerPort request)
+    :path          (.getRequestURI request)
+    :context-path  (.getContextPath request)
+    :servlet-path  (.getServletPath request)
+    :path-info     (.getPathInfo request)
+    :query-string  (.getQueryString request)
+    :parameter-map (->> (.getParameterMap request)
+                        (into {} (map (juxt key (comp vec val)))))
+
+    ;; HTTP
+    :protocol (.getProtocol request)
+    :method   (keyword (.getMethod request))
+
+    ;; Headers
+    :headers            (Headers. request)
+    :header-names       (some->> (.getHeaderNames request)
+                                 (enumeration-seq)
+                                 (vec))
+    :content-length     (.getContentLengthLong request)
+    :content-type       (.getContentType request)
+    :character-encoding (.getCharacterEncoding request)
+    :locales            (some->> (.getLocales request)
+                                 (enumeration-seq)
+                                 (vec))
+    
+    :cookies            (some->> (.getCookies request)
+                                 (seq)
+                                 (vec))
+
+    ;; Body
+    :reader       (.getReader request)
+    :input-stream (.getInputStream request)
+
+    ;; Multipart throws exceptions if servlet is not configured,
+    ;; or if request is malformed.
+    ;; Users should use method rather than ILookup to access parts.
+    ;; :parts        (vec (.getParts request))
+
+    ;; TODO: Trailers fields?
+
+    not-found))
+
+(defn servlet-request-proxy
+  [^HttpServletRequest request]
+  (proxy [HttpServletRequestWrapper clojure.lang.ILookup] [request]
+    (valAt
+      ([key]
+       (servlet-request-lookup this key nil))
+      ([key not-found]
+       (servlet-request-lookup this key not-found)))))
