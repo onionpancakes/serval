0107d4c8aed275a6251e6c0b3c900c3d9ed40cf3
onionpancakes <639985+onionpancakes@users.noreply.github.com>
2022-04-05 23:58:30 -0700

    Added io.channel

---
+++ modules/serval-core/src/dev/onionpancakes/serval/io/channel.clj
@@ -0,0 +1,56 @@
+(ns dev.onionpancakes.serval.io.channel
+  (:import [java.util.concurrent
+            CompletionStage CompletableFuture
+            Flow$Publisher Flow$Subscriber Flow$Subscription]))
+
+(defprotocol ReadChannel
+  (take! [this]))
+
+(deftype FlowSubscriberChannel [^:unsynchronized-mutable
+                                ^Flow$Subscription subscription
+                                ^java.util.Queue buffer
+                                ^long buffer-fill-size
+                                ^java.util.Queue pending
+                                ^long pending-max-size
+                                ^:unsynchronized-mutable
+                                ^boolean closed?
+                                ex-handler]
+  Flow$Subscriber
+  (onSubscribe [this sub]
+    (locking this
+      (if subscription
+        (.cancel subscription))
+      (set! subscription sub)
+      (if (< (.size buffer) buffer-fill-size)
+        (.request subscription 1))))
+  (onNext [this input]
+    (locking this
+      (when-not closed?
+        (if (.peek pending)
+          (.complete ^CompletableFuture (.poll pending) input)
+          (.add buffer input))
+        (if (< (.size buffer) buffer-fill-size)
+          (.request subscription 1)))))
+  (onComplete [this]
+    (locking this
+      (set! closed? (boolean true))))
+  (onError [this throwable]
+    (ex-handler throwable))
+  ReadChannel
+  (take! [this]
+    (locking this
+      (if closed?
+        (CompletableFuture/completedFuture nil)
+        (if-let [val (.poll buffer)]
+          (CompletableFuture/completedFuture val)
+          (if (>= (.size pending) pending-max-size)
+            (throw (ex-info "Exceeded max pending queue size."))
+            (let [cf (CompletableFuture.)]
+              (.add pending cf)
+              cf)))))))
+
+(defn flow-subscriber-channel
+  [n ex-handler]
+  (let [buffer (java.util.LinkedList.)
+        queue  (java.util.LinkedList.)]
+    (FlowSubscriberChannel. nil buffer n queue 1024 false ex-handler)))
