c7a1fa83d086f6cecdf3d249710b2bbc6fe2068b
onionpancakes <639985+onionpancakes@users.noreply.github.com>
2022-04-06 22:38:53 -0700

    removed prototype async body2 code

--- modules/serval-core/src/dev/onionpancakes/serval/io/body2.clj
+++ modules/serval-core/src/dev/onionpancakes/serval/io/body2.clj
@@ -1,81 +1,6 @@
 (ns dev.onionpancakes.serval.io.body2
-  (:import [jakarta.servlet
-            ServletRequest
-            ServletResponse ServletOutputStream
-            WriteListener]
-           [java.nio ByteBuffer]
-           [java.util LinkedList]
-           [java.util.concurrent
-            CompletionStage CompletableFuture
-            Flow$Publisher Flow$Subscriber Flow$Subscription]
-           [java.util.function Function]))
-
-#_(defrecord FlowContext [^ServletOutputStream out
-                        ^java.util.Queue queue
-                        ^java.util.Queue current
-                        ^boolean complete?])
-
-#_(defprotocol FlowSubscriberContext
-  (on-subscribe! [this sub])
-  (on-next! [this input])
-  (on-complete! [this])
-  (process-context! [this]))
-
-#_(deftype ListOfBufferFlowSubscriberContext [^:unsynchronized-mutable ^Flow$Subscription subscription
-                                            ^java.util.Queue queue
-                                            ^:unsynchronized-mutable ^boolean complete?]
-  FlowSubscriberContext
-  (on-subscribe! [_ sub]
-    (if subscription
-      (.cancel subscription))
-    (set! subscription sub))
-  (on-next! [this buffers]
-    (.add queue (LinkedList. buffers)))
-  (on-complete! [this]
-    (set! complete? (boolean true))))
-
-#_(defprotocol Channel
-  (take! [this]))
-
-#_(deftype FlowSubscriberChannel [^:unsynchronized-mutable ^Flow$Subscription subscription
-                                ^java.util.Queue queue
-                                ^java.util.Queue buffer
-                                complete?]
-  Flow$Subscriber
-  (onSubscribe [this sub]
-    (if subscription
-      (.cancel subscription))
-    (set! subscription sub))
-  (onNext [this input]
-    (.complete (.poll queue) input))
-  (onComplete [this])
-  (onError [this throwable])
-  Channel
-  (take! [this]
-    (let [cf (CompletableFuture.)]
-      (.add queue cf)
-      (if subscription
-        (.request subscription 1))
-      cf)))
-
-(defn take! [])
-
-(defn flush-buffers! [out buffers])
-
-(deftype ChannelWriteListener [out ch
-                               ^:unsynchronized-mutable
-                               ^CompletableFuture cf]
-  WriteListener
-  (onWritePossible [this]
-    (let [f (reify Function
-              (apply [this-fn buffers]
-                (if-not (nil? buffers)
-                  (if (flush-buffers! out buffers)
-                    (.thenComposeAsync (take! ch) this-fn)
-                    (CompletableFuture/completedFuture buffers)))))]
-      (set! cf (.thenCompose cf f))))
-  (onError [_ throwable]
-    (.completeExceptionally cf throwable)))
+  (:import [jakarta.servlet ServletResponse]
+           [java.util.concurrent CompletionStage]))
 
 ;; Response body protocol
 
@@ -99,22 +24,12 @@
         (.close this)))
     nil)
   nil
-  (service-body [this _ _ ^ServletResponse response])
+  (service-body [_ _ _ _] nil)
 
   ;; Async
   CompletionStage
   (service-body [this servlet request response]
     (.thenAccept this (reify java.util.function.Consumer
-                        (accept [this value]
+                        (accept [_ body]
                           ;; Value should not be another CompletionStage.
-                          (service-body value servlet request response)))))
-  Flow$Publisher
-  (service-body [this _ ^ServletRequest request ^ServletResponse response]
-    (if-not (.isAsyncStarted request)
-      (.startAsync request))
-    #_(let [out (.getOutputStream response)
-          cf  (CompletableFuture.)
-          wl  (flow-subscriber-write-listener out [] cf false)]
-      (.setWriteListener out wl)
-      (.subscribe this wl)
-      cf)))
+                          (service-body body servlet request response))))))
