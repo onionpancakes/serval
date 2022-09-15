1a3b1d9a721a8ecb161bc2134f7ca9abbd75d7be
onionpancakes <639985+onionpancakes@users.noreply.github.com>
2022-09-15 00:31:52 -0700

    updated dev ns

--- dev/user.clj
+++ dev/user.clj
@@ -1,5 +1,7 @@
 (ns user
   (:require [dev.onionpancakes.serval.core :as srv]
+            [dev.onionpancakes.serval.handler.http
+             :refer [response]]
             [dev.onionpancakes.serval.jetty :as srv.jetty]
             [dev.onionpancakes.serval.reitit :as srv.reitit]
             [dev.onionpancakes.serval.jsonista :as srv.json]
@@ -7,3 +9,27 @@
             [clojure.pprint :refer [pprint]]))
 
 (set! *warn-on-reflection* true)
+
+(defn my-handler
+  [ctx]
+  ;; Assoc the response into ctx map.
+  (into ctx {:serval.response/status       200
+             :serval.response/body         "Hello world!"
+             :serval.response/context-type "text/plain"}))
+
+(defonce server
+  (srv.jetty/server {:connectors [{:protocol :http
+                                   :port     3000}]
+                     :handler    #'my-handler}))
+
+(defn start
+  []
+  (srv.jetty/start server))
+
+(defn stop
+  []
+  (srv.jetty/stop server))
+
+(defn configure
+  [config]
+  (srv.jetty/configure-server! server config))
