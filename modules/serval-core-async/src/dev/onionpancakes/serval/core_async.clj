3bf185b2094a8d62b2427d648b73d9752cb6b9f9
onionpancakes <639985+onionpancakes@users.noreply.github.com>
2022-10-25 21:46:19 -0700

    rm async body impl for core async; added impl for sync body writes

--- modules/serval-core-async/src/dev/onionpancakes/serval/core_async.clj
+++ modules/serval-core-async/src/dev/onionpancakes/serval/core_async.clj
@@ -1,64 +1,22 @@
 (ns dev.onionpancakes.serval.core-async
-  (:require [clojure.core.async :as async :refer [go-loop <! >!]]
-            [dev.onionpancakes.serval.io.body :as io.body]
-            [dev.onionpancakes.serval.io.async :as io.async])
-  (:import [jakarta.servlet ServletRequest ServletResponse WriteListener]
+  (:require [clojure.core.async :as async :refer [<!!]]
+            [dev.onionpancakes.serval.io.body :as io.body])
+  (:import [jakarta.servlet ServletResponse]
            [java.util.concurrent CompletableFuture]))
 
-;; 'cur' is a 1 size channel that holds the current writable.
-
-(deftype ChannelWriteListener [ch cur out ^CompletableFuture cf]
-  WriteListener
-  (onWritePossible [_]
-    (go-loop [buf (<! cur)]
-      (if (some? buf)
-        (if (io.async/write! buf out)
-          (if-some [v (<! ch)]
-            (recur (io.async/writable v))
-            (recur nil))
-          (>! cur buf))
-        (do
-          (async/close! cur)
-          (.complete cf nil)))))
-  (onError [_ throwable]
-    (.completeExceptionally cf throwable)))
-
-(defn channel-write-listener
-  [ch out cf]
-  (let [cur (async/chan 1)
-        ;; Because ChannelWriteListener takes from 'cur' first,
-        ;; we need to initialize it with one value from ch.
-        cb  (fn [x]
-              (if (some? x)
-                (async/put! cur (io.async/writable x))
-                (async/close! cur)))
-        _   (async/take! ch cb)]
-    (ChannelWriteListener. ch cur out cf)))
-
-;; Protocol impl
-
-(defn service-channel-body
-  [ch _ ^ServletRequest request ^ServletResponse response]
-  (let [out (.getOutputStream response)
-        cf  (CompletableFuture.)
-        wl  (channel-write-listener ch out cf)
-        _   (if-not (.isAsyncStarted request)
-              (.startAsync request))
-        _   (.setWriteListener out wl)]
-    cf))
-
-(deftype ChannelBody [ch]
+(defrecord ChannelBody [ch]
   io.body/ResponseBody
-  (service-body [_ servlet request response]
-    (service-channel-body ch servlet request response)))
+  (io.body/service-body [_ _ _ response]
+    (let [out (.getOutputStream ^ServletResponse response)
+          rch (async/thread
+                (loop [value (<!! ch)]
+                  (when (some? value)
+                    (io.body/write value out)
+                    (recur (<!! ch)))))
+          cf  (CompletableFuture.)
+          _   (async/take! rch (fn [_] (.complete cf nil)))]
+      cf)))
 
 (defn channel-body
   [ch]
   (ChannelBody. ch))
-
-(defn extend-channel-as-response-body!
-  []
-  (extend-protocol io.body/ResponseBody
-    clojure.core.async.impl.channels.ManyToManyChannel
-    (service-body [this servlet request response]
-      (service-channel-body this servlet request response))))
--- modules/serval-core-async/test/dev/onionpancakes/serval/tests/core_async/test_core_async.clj
+++ modules/serval-core-async/test/dev/onionpancakes/serval/tests/core_async/test_core_async.clj
@@ -1,33 +1,15 @@
 (ns dev.onionpancakes.serval.tests.core-async.test-core-async
   (:refer-clojure :exclude [send])
-  (:require [dev.onionpancakes.serval.core-async
-             :refer [extend-channel-as-response-body!
-                     channel-body]]
+  (:require [dev.onionpancakes.serval.core-async :as srv.async]
             [dev.onionpancakes.serval.test-utils.server
              :refer [with-response]]
             [dev.onionpancakes.serval.test-utils.client
              :refer [send]]
             [clojure.test :refer [deftest is]]
-            [clojure.core.async :refer [go chan close! >!]]))
+            [clojure.core.async :as async]))
 
-(extend-channel-as-response-body!)
-
-(deftest test-async-writables
-  (with-response {:serval.response/body (go "foo")}
-    (is (= (:body (send)) "foo")))
-  (with-response {:serval.response/body (go (.getBytes "foo"))}
-    (is (= (:body (send)) "foo"))))
-
-(defn test-chan []
-  (let [ch (chan)]
-    (go
-      (doseq [v ["foo" "bar" "baz"]]
-        (>! ch v))
-      (close! ch))
-    ch))
-
-(deftest test-channel-response-body
-  (with-response {:serval.response/body (test-chan)}
-    (is (= (:body (send)) "foobarbaz")))
-  (with-response {:serval.response/body (channel-body (test-chan))}
-    (is (= (:body (send)) "foobarbaz"))))
+(deftest test-channel-body
+  (let [body (-> (async/to-chan! ["foo" "bar" "baz"])
+                 (srv.async/channel-body))]
+    (with-response {:serval.response/body body}
+      (is (= (:body (send)) "foobarbaz")))))
