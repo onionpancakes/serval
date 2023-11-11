556278e5b06803edbccb267fdde7182fc26d1444
onionpancakes <639985+onionpancakes@users.noreply.github.com>
2023-11-11 12:53:16 -0800

    added tests for jetty-test

--- modules/serval-jetty-test/test/dev/onionpancakes/serval/jetty/tests/test_jetty_test.clj
+++ modules/serval-jetty-test/test/dev/onionpancakes/serval/jetty/tests/test_jetty_test.clj
@@ -1,1 +1,23 @@
-(ns dev.onionpancakes.serval.jetty.tests.test-jetty-test)
+(ns dev.onionpancakes.serval.jetty.tests.test-jetty-test
+  (:refer-clojure :exclude [send])
+  (:require [dev.onionpancakes.serval.jetty.test
+             :refer [with-handler with-response send]]
+            [clojure.test :refer [deftest is]]))
+
+(defn example-handler
+  [ctx]
+  {:serval.response/status 200
+   :serval.response/body   "foo"})
+
+(deftest test-with-handler
+  (with-handler example-handler
+    (let [resp (send)]
+      (is (= (:status resp) 200))
+      (is (= (:body resp) "foo")))))
+
+(deftest test-with-response
+  (with-response {:serval.response/status 200
+                  :serval.response/body   "foo"}
+    (let [resp (send)]
+      (is (= (:status resp) 200))
+      (is (= (:body resp) "foo")))))
