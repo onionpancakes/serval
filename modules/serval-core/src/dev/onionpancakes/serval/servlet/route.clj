4a5e44c9f2ed46f3cc5165d8f28ccac1c3e3cb17
onionpancakes <639985+onionpancakes@users.noreply.github.com>
2023-11-15 17:13:40 -0800

    fixed route servlet naming, dedup with url-pattern

--- modules/serval-core/src/dev/onionpancakes/serval/servlet/route.clj
+++ modules/serval-core/src/dev/onionpancakes/serval/servlet/route.clj
@@ -2,17 +2,18 @@
   (:require [dev.onionpancakes.serval.servlet :as srv.servlet]))
 
 (defprotocol RouteServlet
-  (get-servlet-name [this])
+  (get-servlet-name [this url-pattern])
   (add-servlet [this servlet-ctx servlet-name]))
 
 (defprotocol RouteFilter
-  (get-filter-name [this])
-  (add-filter [this servlet-ctx filter-name]))
+  (get-filter-name [this url-pattern])
+  (add-filter [this servlet-ctx filter-name])
+  (get-dispatch-types [this]))
 
 (extend-protocol RouteServlet
   clojure.lang.AFunction
-  (get-servlet-name [this]
-    (str "serval.servlet.route:" (hash this)))
+  (get-servlet-name [this url-pattern]
+    (str "serval.servlet.route/servlet:" (hash this) ":" url-pattern))
   (add-servlet [this servlet-ctx servlet-name]
     (if-some [reg (.getServletRegistration servlet-ctx servlet-name)]
       reg
@@ -20,24 +21,31 @@
 
 (extend-protocol RouteFilter
   clojure.lang.AFunction
-  (get-filter-name [this]
-    (str "serval.servlet.route:" (hash this)))
+  (get-filter-name [this url-pattern]
+    (str "serval.servlet.route/filter:" (hash this) ":" url-pattern))
   (add-filter [this servlet-ctx filter-name]
     (if-some [reg (.getFilterRegistration servlet-ctx filter-name)]
       reg
-      (.addFilter servlet-ctx filter-name (srv.servlet/filter this)))))
+      (.addFilter servlet-ctx filter-name (srv.servlet/filter this))))
+  (get-dispatch-types [this] nil))
 
 (defn add-route
   [servlet-ctx route]
+  {:pre [(vector? route)
+         (>= (count route) 2)]}
   (let [url-pattern   (first route)
         route-servlet (peek route)
-        servlet-name  (get-servlet-name route-servlet)]
+        route-filters (next (pop route))
+        servlet-name  (get-servlet-name route-servlet url-pattern)]
+    ;; Add servlet
     (.. (add-servlet route-servlet servlet-ctx servlet-name)
         (addMapping (into-array String [url-pattern])))
-    (doseq [route-filter (next (pop route))]
-      (let [filter-name (get-filter-name route-filter)]
+    ;; Add filter
+    (doseq [route-filter route-filters]
+      (let [filter-name    (get-filter-name route-filter url-pattern)
+            dispatch-types (get-dispatch-types route-filter)]
         (.. (add-filter route-filter servlet-ctx filter-name)
-            (addMappingForServletName nil true (into-array String [servlet-name])))))
+            (addMappingForServletNames dispatch-types true (into-array String [servlet-name])))))
     servlet-ctx))
 
 (defn add-routes
