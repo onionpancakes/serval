e8aa82c86bf3bd46a271de564e5a2b98eeaea14b
onionpancakes <639985+onionpancakes@users.noreply.github.com>
2023-11-16 23:43:04 -0800

    add servlet util functions for dispatcher type

--- modules/serval-core/src/dev/onionpancakes/serval/servlet.clj
+++ modules/serval-core/src/dev/onionpancakes/serval/servlet.clj
@@ -1,7 +1,9 @@
 (ns dev.onionpancakes.serval.servlet
   (:refer-clojure :exclude [filter])
   (:require [dev.onionpancakes.serval.impl.http.servlet :as impl.http.servlet]
-            [dev.onionpancakes.serval.impl.http.filter :as impl.http.filter]))
+            [dev.onionpancakes.serval.impl.http.filter :as impl.http.filter])
+  (:import [jakarta.servlet DispatcherType]
+           [java.util EnumSet]))
 
 (defn servlet
   "Creates a Servlet which services a http response from the handler function."
@@ -13,3 +15,22 @@
   ^jakarta.servlet.GenericFilter
   [handler]
   (impl.http.filter/filter handler))
+
+(defn as-dispatcher-type
+  ^DispatcherType
+  [k]
+  (case k
+    :async   DispatcherType/ASYNC
+    :error   DispatcherType/ERROR
+    :forward DispatcherType/FORWARD
+    :include DispatcherType/INCLUDE
+    :request DispatcherType/REQUEST
+    (if (instance? DispatcherType k)
+      k
+      (throw (ex-info "Neither valid keyword or DispatcherType." {:arg k})))))
+
+(defn dispatcher-type-enum-set
+  ^EnumSet
+  [types]
+  (doto (EnumSet/noneOf DispatcherType)
+    (.addAll (mapv as-dispatcher-type types))))
