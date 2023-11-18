61d35e190d694d74ee1b07fc363dcb4ccf355c0b
onionpancakes <639985+onionpancakes@users.noreply.github.com>
2023-11-17 21:16:54 -0800

    rm unused imports in servlet

--- modules/serval-core/src/dev/onionpancakes/serval/servlet.clj
+++ modules/serval-core/src/dev/onionpancakes/serval/servlet.clj
@@ -1,9 +1,7 @@
 (ns dev.onionpancakes.serval.servlet
   (:refer-clojure :exclude [filter])
   (:require [dev.onionpancakes.serval.impl.http.servlet :as impl.http.servlet]
-            [dev.onionpancakes.serval.impl.http.filter :as impl.http.filter])
-  (:import [jakarta.servlet DispatcherType]
-           [java.util EnumSet]))
+            [dev.onionpancakes.serval.impl.http.filter :as impl.http.filter]))
 
 (defn servlet
   "Creates a Servlet which services a http response from the handler function."
