4a46c9c61c8e2640231f7a57bcd081cccac1a9a3
onionpancakes <639985+onionpancakes@users.noreply.github.com>
2023-11-20 20:43:44 -0800

    more performant var args overload for servlet and filter

--- modules/serval-core/src/dev/onionpancakes/serval/impl/http/filter.clj
+++ modules/serval-core/src/dev/onionpancakes/serval/impl/http/filter.clj
@@ -14,20 +14,49 @@
    :serval.context/filter-chain filter-chain})
 
 (defn do-filter-chain
-  [^FilterChain filter-chain do-chain default-request default-response]
-  (if do-chain
+  [^FilterChain filter-chain m default-request default-response]
+  (if-let [do-chain (:serval.filter/do-filter-chain m)]
     (if (vector? do-chain)
       (.doFilter filter-chain (nth do-chain 0) (nth do-chain 1))
       (.doFilter filter-chain default-request default-response))))
 
 (defn service-fn
-  [handler & args]
-  (fn [filter request response ^FilterChain filter-chain]
-    (let [ctx (context filter request response filter-chain)
-          ret (apply handler ctx args)]
-      (response.http/set-response response ret)
-      (let [do-chain (:serval.filter/do-filter-chain ret)]
-        (do-filter-chain filter-chain do-chain request response)))))
+  ([handler]
+   (fn [filter request response ^FilterChain filter-chain]
+     (let [ctx (context filter request response filter-chain)
+           ret (handler ctx)]
+       (response.http/set-response response ret)
+       (do-filter-chain filter-chain ret request response))))
+  ([handler a]
+   (fn [filter request response ^FilterChain filter-chain]
+     (let [ctx (context filter request response filter-chain)
+           ret (handler ctx a)]
+       (response.http/set-response response ret)
+       (do-filter-chain filter-chain ret request response))))
+  ([handler a b]
+   (fn [filter request response ^FilterChain filter-chain]
+     (let [ctx (context filter request response filter-chain)
+           ret (handler ctx a b)]
+       (response.http/set-response response ret)
+       (do-filter-chain filter-chain ret request response))))
+  ([handler a b c]
+   (fn [filter request response ^FilterChain filter-chain]
+     (let [ctx (context filter request response filter-chain)
+           ret (handler ctx a b c)]
+       (response.http/set-response response ret)
+       (do-filter-chain filter-chain ret request response))))
+  ([handler a b c d]
+   (fn [filter request response ^FilterChain filter-chain]
+     (let [ctx (context filter request response filter-chain)
+           ret (handler ctx a b c d)]
+       (response.http/set-response response ret)
+       (do-filter-chain filter-chain ret request response))))
+  ([handler a b c d & args]
+   (fn [filter request response ^FilterChain filter-chain]
+     (let [ctx (context filter request response filter-chain)
+           ret (apply handler ctx a b c d args)]
+       (response.http/set-response response ret)
+       (do-filter-chain filter-chain ret request response)))))
 
 (defn filter
   ^GenericFilter
--- modules/serval-core/src/dev/onionpancakes/serval/impl/http/servlet.clj
+++ modules/serval-core/src/dev/onionpancakes/serval/impl/http/servlet.clj
@@ -16,11 +16,36 @@
    :serval.context/response response})
 
 (defn service-fn
-  [handler & args]
-  (fn [servlet request response]
-    (let [ctx (context servlet request response)
-          ret (apply handler ctx args)]
-      (response.http/set-response response ret))))
+  ([handler]
+   (fn [servlet request response]
+     (let [ctx (context servlet request response)
+           ret (handler ctx)]
+       (response.http/set-response response ret))))
+  ([handler a]
+   (fn [servlet request response]
+     (let [ctx (context servlet request response)
+           ret (handler ctx a)]
+       (response.http/set-response response ret))))
+  ([handler a b]
+   (fn [servlet request response]
+     (let [ctx (context servlet request response)
+           ret (handler ctx a b)]
+       (response.http/set-response response ret))))
+  ([handler a b c]
+   (fn [servlet request response]
+     (let [ctx (context servlet request response)
+           ret (handler ctx a b c)]
+       (response.http/set-response response ret))))
+  ([handler a b c d]
+   (fn [servlet request response]
+     (let [ctx (context servlet request response)
+           ret (handler ctx a b c d)]
+       (response.http/set-response response ret))))
+  ([handler a b c d & args]
+   (fn [servlet request response]
+     (let [ctx (context servlet request response)
+           ret (apply handler ctx a b c d args)]
+       (response.http/set-response response ret)))))
 
 (defn servlet
   ^GenericServlet
