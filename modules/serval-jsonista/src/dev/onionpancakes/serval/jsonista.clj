cd8c344912c5a99789bd7f6a2188b8e5f322cbae
onionpancakes <639985+onionpancakes@users.noreply.github.com>
2024-02-10 23:34:54 -0800

    rm read-json and JsonBody type. refactored json-body to json-writable

--- modules/serval-jsonista/src/dev/onionpancakes/serval/jsonista.clj
+++ modules/serval-jsonista/src/dev/onionpancakes/serval/jsonista.clj
@@ -1,44 +1,10 @@
 (ns dev.onionpancakes.serval.jsonista
   (:require [dev.onionpancakes.serval.response.body
              :as response.body]
-            [jsonista.core :as json])
-  (:import [jakarta.servlet ServletRequest ServletResponse]))
+            [jsonista.core :as json]))
 
-(def default-object-mapper
-  json/default-object-mapper)
-
-(def keyword-keys-object-mapper
-  json/keyword-keys-object-mapper)
-
-;; Read
-
-(defn read-json
-  ([ctx]
-   (read-json ctx nil))
-  ([ctx {:keys [object-mapper value-key error-key]
-         :or   {object-mapper default-object-mapper
-                value-key     :serval.jsonista/value
-                error-key     :serval.jsonista/error}}]
-   (try
-     (let [value (-> ^ServletRequest (:serval.context/request ctx)
-                     (.getReader)
-                     (json/read-value object-mapper))]
-       (assoc ctx value-key value))
-     (catch Throwable ex
-       (assoc ctx error-key ex)))))
-
-;; Write
-
-(deftype JsonBody [value object-mapper]
-  response.body/WritableToWriter
-  (value-write-to-writer [_ writer]
-    (json/write-value writer value object-mapper))
-  response.body/Body
-  (body-write-to-response [this response]
-    (.value-write-to-writer this (.getWriter ^ServletResponse response))))
-
-(defn json-body
+(defn json-writable
   ([value]
-   (JsonBody. value default-object-mapper))
+   (response.body/writable-to-writer value json/write-value))
   ([value object-mapper]
-   (JsonBody. value object-mapper)))
+   (response.body/writable-to-writer value json/write-value object-mapper)))
--- modules/serval-jsonista/test/dev/onionpancakes/serval/jsonista/tests/test_jsonista.clj
+++ modules/serval-jsonista/test/dev/onionpancakes/serval/jsonista/tests/test_jsonista.clj
@@ -4,65 +4,13 @@
              :refer [with-response send]]
             [dev.onionpancakes.serval.jsonista :as srv.json]
             [clojure.test :refer [deftest is]]
-            [jsonista.core :as json])
-  (:import [jakarta.servlet ServletRequest]
-           [java.io BufferedReader CharArrayReader CharArrayWriter]))
+            [jsonista.core :as json]))
 
-(defn json-reader
-  [value]
-  (with-open [wtr (doto (CharArrayWriter.)
-                    (json/write-value value))]
-    (-> (CharArrayReader. (.toCharArray wtr))
-        (BufferedReader.))))
-
-(defn context-with-json
-  [value]
-  {:serval.context/request (reify ServletRequest
-                             (getReader [this]
-                               (json-reader value)))})
-
-(deftest test-read-json
+(deftest test-json-writable
   (let [value {"foo" "bar"}
-        ctx   (context-with-json value)
-        ret   (srv.json/read-json ctx)]
-    (is (= (:serval.jsonista/value ret) value)))
-
-  ;; Custom value key
-  (let [value {"foo" "bar"}
-        ctx   (context-with-json value)
-        ret   (srv.json/read-json ctx {:value-key :value})]
-    (is (= (:value ret) value)))
-
-  ;; Keyword keys object mapper
-  (let [value {:foo "bar"}
-        ctx   (context-with-json value)
-        ret   (srv.json/read-json ctx {:object-mapper srv.json/keyword-keys-object-mapper})]
-    (is (= (:serval.jsonista/value ret) value))))
-
-(defn string-reader
-  [^String value]
-  (-> (CharArrayReader. (.toCharArray value))
-      (BufferedReader.)))
-
-(defn context-with-string
-  [value]
-  {:serval.context/request (reify ServletRequest
-                             (getReader [this]
-                               (string-reader value)))})
-
-(deftest test-read-json-error
-  (let [ctx (context-with-string "{foo")
-        ret (srv.json/read-json ctx)]
-    (is (:serval.jsonista/error ret)))
-
-  ;; Custom error key
-  (let [ctx (context-with-string "{foo")
-        ret (srv.json/read-json ctx {:error-key :error})]
-    (is (:error ret))))
-
-(deftest test-json-body
-  (let [value {"foo" "bar"}]
-    (with-response {:serval.response/body (srv.json/json-body value)}
-      (let [resp       (send)
-            resp-value (json/read-value (:body resp))]
-        (is (= resp-value value))))))
+        body  (srv.json/json-writable value)]
+    (with-response {:serval.response/body body}
+      (let [result (-> (send)
+                       (:body)
+                       (json/read-value))]
+        (is (= value result))))))
