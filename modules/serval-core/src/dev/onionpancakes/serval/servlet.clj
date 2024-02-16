c5a36960680e9908c0cbc1f9fa3b41240c363506
onionpancakes <639985+onionpancakes@users.noreply.github.com>
2024-02-15 23:13:31 -0800

    refactored servlet ns, pred filter impl

--- modules/serval-core/src/dev/onionpancakes/serval/servlet.clj
+++ modules/serval-core/src/dev/onionpancakes/serval/servlet.clj
@@ -1,51 +1,42 @@
 (ns dev.onionpancakes.serval.servlet
   (:refer-clojure :exclude [filter])
   (:require [dev.onionpancakes.serval.impl.servlet :as impl.servlet]
-            [dev.onionpancakes.serval.impl.filter :as impl.filter]
-            [dev.onionpancakes.serval.core :as srv]))
+            [dev.onionpancakes.serval.impl.filter :as impl.filter])
+  (:import [jakarta.servlet FilterChain]
+           [jakarta.servlet.http HttpServletResponse]))
 
-(defn servlet
-  "Creates a Servlet which services a http response from the handler function."
-  ^jakarta.servlet.GenericServlet
-  [handler & args]
-  (apply impl.servlet/servlet handler args))
+(def servlet
+  impl.servlet/servlet)
 
-(defn filter
-  ^jakarta.servlet.GenericFilter
-  [handler & args]
-  (apply impl.filter/filter handler args))
+(def filter
+  impl.filter/filter)
 
 ;; Filters
 
-(defn pred-filter-handler
-  ([ctx pred code]
-   #_
-   (if (pred ctx)
-     (srv/do-filter-chain! ctx)
-     (srv/send-error! ctx code)))
-  ([ctx pred code message]
-   #_
-   (if (pred ctx)
-     (srv/do-filter-chain! ctx)
-     (srv/send-error! ctx code message))))
+(defn pred-do-filter-fn
+  ([pred code]
+   (fn [_ request ^HttpServletResponse response ^FilterChain chain]
+     (if (pred request)
+       (.doFilter chain request response)
+       (.sendError response code))))
+  ([pred code message]
+   (fn [_ request ^HttpServletResponse response ^FilterChain chain]
+     (if (pred request)
+       (.doFilter chain request response)
+       (.sendError response code message)))))
 
 (defn pred-filter
-  (^jakarta.servlet.GenericFilter
-   [pred code]
-   (filter pred-filter-handler pred code))
-  (^jakarta.servlet.GenericFilter
-   [pred code message]
-   (filter pred-filter-handler pred code message)))
+  ([pred code]
+   (impl.filter/filter (pred-do-filter-fn pred code)))
+  ([pred code message]
+   (impl.filter/filter (pred-do-filter-fn pred code message))))
 
 (defn http-method-filter
-  (^jakarta.servlet.GenericFilter
-   [allowed-method?]
+  ([allowed-method?]
    (http-method-filter allowed-method? 405))
-  (^jakarta.servlet.GenericFilter
-   [allowed-method? code]
-   (-> (comp allowed-method? :method :serval.context/request)
+  ([allowed-method? code]
+   (-> (comp allowed-method? :method)
        (pred-filter code)))
-  (^jakarta.servlet.GenericFilter
-   [allowed-method? code message]
-   (-> (comp allowed-method? :method :serval.context/request)
+  ([allowed-method? code message]
+   (-> (comp allowed-method? :method)
        (pred-filter code message))))
