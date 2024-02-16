aac781f977ed66b7581e01c9b1c2ba567ff54824
onionpancakes <639985+onionpancakes@users.noreply.github.com>
2024-02-15 23:47:28 -0800

    tests for servlet ns

--- modules/serval-core/test/dev/onionpancakes/serval/core/tests/test_servlet.clj
+++ modules/serval-core/test/dev/onionpancakes/serval/core/tests/test_servlet.clj
@@ -6,4 +6,43 @@
              :refer [with-handler send]]
             [clojure.test :refer [deftest is]]))
 
+(defn example-service-fn
+  [_ req resp]
+  (srv/write-body resp "foo"))
 
+(deftest test-servlet
+  (with-handler (srv.servlet/servlet example-service-fn)
+    (let [resp (send)]
+      (is (= (:body resp) "foo")))))
+
+(defn example-do-filter-fn
+  [_ _ resp _]
+  (srv/write-body resp "foo"))
+
+(deftest test-filter
+  (with-handler {:routes [["/*" (srv.servlet/filter example-do-filter-fn) example-service-fn]]}
+    (let [resp (send)]
+      (is (= (:body resp) "foo")))))
+
+(defn example-pred
+  [request]
+  (case (:method request)
+    :GET  true
+    :POST false
+    false))
+
+(deftest test-pred-filter
+  (with-handler {:routes [["/*" (srv.servlet/pred-filter example-pred 400) example-service-fn]]}
+    (let [resp (send)]
+      (is (= (:status resp) 200))
+      (is (= (:body resp) "foo")))
+    (let [resp (send {:method :POST})]
+      (is (= (:status resp) 400)))))
+
+(deftest test-http-method-filter
+  (with-handler {:routes [["/*" (srv.servlet/http-method-filter #{:GET}) example-service-fn]]}
+    (let [resp (send)]
+      (is (= (:status resp) 200))
+      (is (= (:body resp) "foo")))
+    (let [resp (send {:method :POST})]
+      (is (= (:status resp) 405)))))
